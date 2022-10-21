7d389d9702ce1ef93a2fe27dbef4d743db0a84d5
CipherKill <37742577+CipherKill@users.noreply.github.com>
2022-10-21 11:29:18 +0530

    Commented a useless code

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -9,18 +9,18 @@ const {
 router.route('/register-new-admin').post(registerNewAdmin); 
 router.route('/login').post(login)
 
-//delete the below later
-router.route('/checkpassword').post((req,res)=>{
-    const realPassword=process.env.ADMIN_PASS;
-    const receivedPassword=req.body.key;
-    if(realPassword===receivedPassword){
-        res.status(200).json(1);
-    }
-    else{
-        res.status(401);
-        throw new Error('Bad credentials.');
-    }
-})
+// //delete the below later
+// router.route('/checkpassword').post((req,res)=>{
+//     const realPassword=process.env.ADMIN_PASS;
+//     const receivedPassword=req.body.key;
+//     if(realPassword===receivedPassword){
+//         res.status(200).json(1);
+//     }
+//     else{
+//         res.status(401);
+//         throw new Error('Bad credentials.');
+//     }
+// })
 
 
 
